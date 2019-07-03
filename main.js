4fbc9ee614c71b530f46100a798a9e0e9da89cfb
Jonathan Robertson <42648270+JonRobertson93@users.noreply.github.com>
2019-07-03 11:18:55 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-$('#toiletRatings input:radio').addClass('input_hidden');
-$('#toiletRatings label').click(function(){
-	$(this).addClass('selected').siblings().removeClass('selected');
-	$(this).previousSibling.addClass('selected');
-});
